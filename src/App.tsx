67405ab7c3238561816f7ec1efa5dfe1b894ca03
P4CM4N890 <p4cm4n890@gmail.com>
2024-06-05 21:18:48 -0600

    Rutas de navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./components";
+import { Contact, News, People, StudentProjects } from "./pages";
 
 const App = () => {
     return (
         <>
-            
+            <Header/>
+            <Routes>
+                <Route path='/' element={ <News/> } />
+                <Route path='/people' element={ <People/> } />
+                <Route path='/student-projects' element={ <StudentProjects/> } />
+                <Route path='/contact' element={ <Contact/> } />
+            </Routes>
         </>
     )
 }
