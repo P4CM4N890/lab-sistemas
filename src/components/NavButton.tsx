d23f80e95d08d7b8d62d8361236dbb472ac0fdd5
P4CM4N890 <p4cm4n890@gmail.com>
2024-06-13 18:40:21 -0600

    Modificación de estilo del botón

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -14,7 +14,7 @@ export const NavButton = ( { label = 'Button', to = '', onClick } : Props ) => {
         <NavLink  
             to={ `/${ to }` } 
             onClick={ onClick }
-            className={ ({ isActive }) => `${ isActive ? btnActive : 'text-stone-500' } flex justify-center items-center grow md:grow-0 text-xl md:text-lg truncate py-2 px-6  border-t-2 md:border-b-2 md:border-transparent border-t-black md:hover:border-b-black hover:text-black` }
+            className={ ({ isActive }) => `${ isActive ? btnActive : 'text-stone-500' } flex justify-center items-center grow md:grow-0 text-xl md:text-lg truncate py-2 px-6  border-t md:border-b-2 md:border-transparent border-t-stone-800 md:hover:border-b-black hover:text-black` }
         >
             { label }
         </NavLink>
