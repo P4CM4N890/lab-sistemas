c4eb8ae4eeef1707cb53d04aa5d2002711ba066d
P4CM4N890 <p4cm4n890@gmail.com>
2024-06-13 18:39:48 -0600

    Añadición de metadatos

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -3,6 +3,11 @@ import { Icon } from "../../interfaces"
 export const CloseIcon = ( { extraClass } : Icon ) => {
   return (
     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1.5"  strokeLinecap="round"  strokeLinejoin="round" className={ `${ extraClass }` }  >
+        <title>Cerrar</title>
+        <desc>Icono de una equis para cerrar</desc>
+
+        <rect fill="none" stroke="none" width="24" height="24"/>
+
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M18 6l-12 12" />
         <path d="M6 6l12 12" />
--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -3,6 +3,11 @@ import { Icon } from "../../interfaces"
 export const MenuIcon = ( { extraClass } : Icon ) => {
   return (
     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1.5"  strokeLinecap="round"  strokeLinejoin="round" className={ `${ extraClass }` } >
+        <title>Menú</title>
+        <desc>Icono de menú</desc>
+
+        <rect fill="none" stroke="none" width="24" height="24"/>
+
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M4 6l16 0" /><path d="M4 12l16 0" />
         <path d="M4 18l16 0" />
