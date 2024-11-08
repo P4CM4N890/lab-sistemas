478012ae64b3fcc5eeeab64833983504c01f8c7f
P4CM4N890 <p4cm4n890@gmail.com>
2024-11-07 18:38:09 -0600

    Redirección de páginas inexistentes a la principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Footer, Header } from "./components";
 import { Contact, News, People, StudentProjects } from "./pages";
 
@@ -10,7 +10,7 @@ const App = () => {
                 <Route path='/' element={ <News/> } />
                 <Route path='/people' element={ <People/> } />
                 <Route path='/student-projects' element={ <StudentProjects/> } />
-                <Route path='/contact' element={ <Contact/> } />
+                <Route path='*' element={ <Navigate to='/' replace /> } />
             </Routes>
             <Footer/>
         </>
